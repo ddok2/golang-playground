475bc77983284f6c8becfabf2c66d587372bf918
Sungyub NA <darkerkorean@gmail.com>
2020-01-15 20:07:37 +0900

    feat(ch1/dup2): 표준입력 또는 파일 목록 가져오기
    add dup2

---
+++ ch1/dup2/main.go
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+ *
+ * - main.go
+ * - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	// input.Err()에서 잠재적 오류 무시
+}
