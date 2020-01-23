508810fe7d1201e2c1b51cab9127064133709dd8
Sungyub NA <darkerkorean@gmail.com>
2020-01-23 11:20:00 +0900

    feat(ch4/append): append 함수
    페이지 112

--- ch4/append/main.go
+++ ch4/append/main.go
@@ -19,6 +19,19 @@ func main() {
 	}
 }
 
+/**
+0  cap=1	[0]
+1  cap=2	[0 1]
+2  cap=4	[0 1 2]
+3  cap=4	[0 1 2 3]
+4  cap=8	[0 1 2 3 4]
+5  cap=8	[0 1 2 3 4 5]
+6  cap=8	[0 1 2 3 4 5 6]
+7  cap=8	[0 1 2 3 4 5 6 7]
+8  cap=16	[0 1 2 3 4 5 6 7 8]
+9  cap=16	[0 1 2 3 4 5 6 7 8 9]
+*/
+
 func appendInt(x []int, y int) []int {
 	var z []int
 
