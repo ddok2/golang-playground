37b3d6841723f724f106165185d152e3a72a13e6
Sungyub NA <darkerkorean@gmail.com>
2020-01-24 09:00:00 +0900

    feat(ch2/tempconv): 패키지와 파일
    페이지 65

---
+++ ch2/tempconv/tempconv.go
@@ -0,0 +1,25 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - tempconv.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F", f)
+}
