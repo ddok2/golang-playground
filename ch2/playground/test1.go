359fe3002859f4b6c26e9482e0ec1809b6e1a5c4
Sungyub NA <darkerkorean@gmail.com>
2020-01-31 09:00:00 +0900

    feat(ch2/playground): 범위
    페이지 70

---
+++ ch2/playground/test1.go
@@ -0,0 +1,19 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - test1.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+import "fmt"
+
+func f() {}
+
+var g = "g"
+
+func main() {
+	f := "f"
+	fmt.Println(f)
+	fmt.Println(g)
+	// fmt.Println(h)
+}
