9fc9a8524cc7b96f5ee772a3757c5fdff7e9aaec
Sungyub NA <darkerkorean@gmail.com>
2020-02-11 19:40:10 +0900

    feat(ch3/printints): 문자열과 바이트슬라이스
    페이지 98

---
+++ ch3/printints/main.go
@@ -0,0 +1,29 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - main.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func intsToString(values []int) string {
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		fmt.Fprintf(&buf, "%d", v)
+	}
+	buf.WriteByte(']')
+
+	return buf.String()
+}
+
+func main() {
+	fmt.Println(intsToString([]int{1, 2, 3}))
+}
