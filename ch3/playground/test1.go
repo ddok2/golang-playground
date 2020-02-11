cca91baf4901b9d3ae28eb1ca23b1e6e752a1497
Sungyub NA <darkerkorean@gmail.com>
2020-02-11 19:17:58 +0900

    feat(ch3/playgorund): 배열,슬라이스,맵
    책에 없는 내용,

---
+++ ch3/playground/test1.go
@@ -0,0 +1,17 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - test1.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+import "fmt"
+
+// 6.1 배열
+
+func main() {
+	var x [5]int
+	x[4] = 100
+
+	fmt.Println(x)
+}
