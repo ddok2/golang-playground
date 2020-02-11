28cdbb6ab2155f23961d532aa7474f6831950eea
Sungyub NA <darkerkorean@gmail.com>
2020-02-11 19:19:43 +0900

    feat(ch3/playgorund): 배열,슬라이스,맵
    책에 없는 내용,

--- ch3/playground/test1.go
+++ ch3/playground/test1.go
@@ -10,8 +10,17 @@ import "fmt"
 // 6.1 배열
 
 func main() {
-	var x [5]int
-	x[4] = 100
+	var x [5]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+	x[3] = 82
+	x[4] = 83
 
-	fmt.Println(x)
+	var total float64 = 0
+	for _, v := range x {
+		total += v
+	}
+
+	fmt.Println(total / 5)
 }
