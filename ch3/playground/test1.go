9879207a6e1e01a226ecb41ee9d94dc56e244dda
Sungyub NA <darkerkorean@gmail.com>
2020-02-11 19:27:56 +0900

    feat(ch3/playgorund): 배열,슬라이스,맵
    책에 없는 내용, 슬라이스

--- ch3/playground/test1.go
+++ ch3/playground/test1.go
@@ -11,7 +11,9 @@ import "fmt"
 
 func main() {
 	slice1 := []int{1, 2, 3}
-	slice2 := append(slice1, 4, 5, 6, 7, 8, 9)
+	slice2 := make([]int, 2)
+
+	copy(slice2, slice1)
 
 	fmt.Println(slice1, slice2)
 }
