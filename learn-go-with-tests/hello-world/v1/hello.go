488d40d41e3705282c04434f54e3593a7bcde750
Sungyub NA <darkerkorean@gmail.com>
2020-03-05 18:50:09 +0900

    feat(learn-go-with-tests): hello-world
    v1

---
+++ learn-go-with-tests/hello-world/v1/hello.go
@@ -0,0 +1,16 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - hello.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, Sung:)"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
---
+++ learn-go-with-tests/hello-world/v1/hello_test.go
@@ -0,0 +1,17 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - hello_test.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, Sung:)"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
