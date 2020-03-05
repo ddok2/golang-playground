6256f93e354295ac0bc9ac048389f6981e8b7911
Sungyub NA <darkerkorean@gmail.com>
2020-03-05 18:59:05 +0900

    feat(learn-go-with-tests): hello-world
    v3

--- learn-go-with-tests/hello-world/v1/hello_test.go
+++ learn-go-with-tests/hello-world/v1/hello_test.go
@@ -8,10 +8,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sung")
-	want := "Hello, Sung"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Sung")
+		want := "Hello, Sung"
+
+		if got != want {
+			t.Errorf("got: %q \nwant: %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empth string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got: %q \nwant: %q", got, want)
+		}
+	})
 }
