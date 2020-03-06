b0c46968d174ddff87ab38da2e233d7f2bb7fa79
Sungyub NA <darkerkorean@gmail.com>
2020-03-06 09:00:00 +0900

    feat(learn-go-with-tests): integers
    v1

---
+++ learn-go-with-tests/integers/adder.go
@@ -0,0 +1,10 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - adder.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package integers
+
+func Add(x, y int) int {
+	return x + y
+}
---
+++ learn-go-with-tests/integers/adder_test.go
@@ -0,0 +1,17 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - adder_test.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
