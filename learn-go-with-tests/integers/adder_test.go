0cead404073e3053565aa8872f85e6a423252b8d
Sungyub NA <darkerkorean@gmail.com>
2020-03-13 16:08:49 +0900

    feat(learn-go-with-tests): integers
    v8

--- learn-go-with-tests/integers/adder_test.go
+++ learn-go-with-tests/integers/adder_test.go
@@ -7,6 +7,7 @@ package integers
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -25,25 +26,86 @@ import (
 // 	// Output: 6
 // }
 
-func TestAdd(t *testing.T) {
+// func TestAdd(t *testing.T) {
+//
+// 	f := make([]interface{}, 0)
+//
+// 	f = append(f, add("sung"))
+// 	f = append(f, add("yub"))
+// 	f = append(f, add("na"))
+//
+// 	lang := make([]string, 0)
+// 	for _, v := range f {
+// 		result := v.(func() string)()
+// 		lang = append(lang, result)
+// 	}
+//
+// 	fmt.Println(lang)
+//
+// }
+//
+// func add(args string) string {
+//
+// 	return args
+// }
 
-	f := make([]interface{}, 0)
+func TestMulti(t *testing.T) {
 
-	f = append(f, add("sung"))
-	f = append(f, add("yub"))
-	f = append(f, add("na"))
+	testArr := make([]interface{}, 0)
 
-	lang := make([]string, 0)
-	for _, v := range f {
-		result := v.(func() string)()
-		lang = append(lang, result)
+	testArr = append(testArr, &typeA{
+		name:    "a",
+		number1: 1,
+		number2: 2,
+	})
+
+	testArr = append(testArr, &typeB{
+		name:    "B1",
+		number:  2,
+		number2: 0,
+	})
+
+	testArr = append(testArr, &typeB{
+		name:    "B2",
+		number:  4,
+		number2: 0,
+	})
+
+	for i, v := range testArr {
+		t := reflect.TypeOf(v).String()
+		switch t {
+		case "*integers.typeA":
+			fmt.Println("this is a")
+			fmt.Println(v.(*typeA).Add())
+			fmt.Println(i)
+
+			break
+
+		case "*integers.typeB":
+			fmt.Println("this is b")
+			fmt.Println(v)
+			// fmt.Println(testArr[i].(typeB).Sub())
+			break
+		}
 	}
-
-	fmt.Println(lang)
-
 }
 
-func add(args string) string {
+type typeA struct {
+	name    string
+	number1 int
+	number2 int
+}
 
-	return args
+func (a *typeA) Add() int {
+	return a.number1 + a.number2
+}
+
+type typeB struct {
+	name    string
+	number  int
+	number2 int
+}
+
+func (b *typeB) Sub() int {
+	return b.number2 - b.number
 }
