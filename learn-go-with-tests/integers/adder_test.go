09f7d60cb13f0c6b44005a4f65ad2ad085a4b978
Sungyub NA <darkerkorean@gmail.com>
2020-03-13 15:07:21 +0900

    feat(learn-go-with-tests): integers
    v7

--- learn-go-with-tests/integers/adder_test.go
+++ learn-go-with-tests/integers/adder_test.go
@@ -10,17 +10,40 @@ import (
 	"testing"
 )
 
-func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+// func TestAdder(t *testing.T) {
+// 	sum := Add(2, 2)
+// 	expected := 4
+//
+// 	if sum != expected {
+// 		t.Errorf("expected '%d' but got '%d'", expected, sum)
+// 	}
+// }
+//
+// func ExampleAdder() {
+// 	sum := Add(1, 5)
+// 	fmt.Println(sum)
+// 	// Output: 6
+// }
 
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
+func TestAdd(t *testing.T) {
+
+	f := make([]interface{}, 0)
+
+	f = append(f, add("sung"))
+	f = append(f, add("yub"))
+	f = append(f, add("na"))
+
+	lang := make([]string, 0)
+	for _, v := range f {
+		result := v.(func() string)()
+		lang = append(lang, result)
 	}
+
+	fmt.Println(lang)
+
 }
 
-func ExampleAdder() {
-	sum := Add(1, 5)
-	fmt.Println(sum)
-	// Output: 6
+func add(args string) string {
+
+	return args
 }
