395b21bcac3a52f256ecf1dab238a4de3833f671
Sungyub NA <darkerkorean@gmail.com>
2020-03-30 09:00:00 +0900

    feat(learn-go-with-tests): iteration
    v2

--- learn-go-with-tests/iteration/repeat_test.go
+++ learn-go-with-tests/iteration/repeat_test.go
@@ -15,3 +15,11 @@ func TestRepaet(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated = repeated + character
+	}
+	return repeated
+}
