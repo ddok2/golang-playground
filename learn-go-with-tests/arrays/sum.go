4e116e37ccc097fd36ea6f68377add6979412108
Sungyub NA <darkerkorean@gmail.com>
2020-04-02 08:42:15 +0900

    feat(learn-go-with-tests): Arrays and slices
    v2

---
+++ learn-go-with-tests/arrays/sum.go
@@ -0,0 +1,10 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - sum.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+func Sum(numbers [5]int) int {
+	return 0
+}
