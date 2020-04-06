534609be0e27affb1ae62b8ce43a124366bf7871
Sungyub NA <darkerkorean@gmail.com>
2020-04-06 10:14:22 +0900

    feat(learn-go-with-tests): Arrays and slices
    v8

--- learn-go-with-tests/arrays/sum_test.go
+++ learn-go-with-tests/arrays/sum_test.go
@@ -10,10 +10,11 @@ import "testing"
 func TestSum(t *testing.T) {
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
+
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
