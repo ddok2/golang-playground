97fd9404a6efb7ab9be4182f552e4fcf5c02987b
Sungyub NA <darkerkorean@gmail.com>
2020-04-11 19:07:54 +0900

    feat(learn-go-with-tests): Arrays and slices
    v11

--- learn-go-with-tests/arrays/sum_test.go
+++ learn-go-with-tests/arrays/sum_test.go
@@ -5,7 +5,10 @@
 
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -37,7 +40,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
