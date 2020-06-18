cb194b28b0499413b61bf367f29d1217b0ec5495
Sungyub NA <darkerkorean@gmail.com>
2020-06-19 08:56:24 +0900

    feat(레이스컨디션): concurrency-in-go
    ch1 3번째

--- concurrency-in-go/ch1/raceConditions.go
+++ concurrency-in-go/ch1/raceConditions.go
@@ -21,7 +21,12 @@ func main() {
 
 	if data == 0 {
 		fmt.Printf("the value is %v. \n", data)
+	} else {
+		fmt.Printf("the value is %v. \n", data)
 	}
 }
 
 // line 20은 잘못되었다.
+// data 변수를 증가시키는 goroutine
+// data 값이 0인지 확인하는 if 구문
+// 출력할 data의 값을 가져오는 fmt.Printf 구문
