ec38d6892ffe3f4f6e232b89db995dfc10baa323
Sungyub NA <darkerkorean@gmail.com>
2020-07-01 09:16:08 +0900

    feat(learn-go-with-tests): Structs, methods & interfaces
    v2

---
+++ learn-go-with-tests/struct-interfaces/shapes.go
@@ -0,0 +1,10 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - shapes.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
