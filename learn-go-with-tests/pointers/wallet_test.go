6d2dde30bc31897fe44505c059893700941f63c3
Sungyub NA <darkerkorean@gmail.com>
2020-07-08 13:26:45 +0900

    feat(learn-go-with-tests): pointers
    v1

---
+++ learn-go-with-tests/pointers/wallet_test.go
@@ -0,0 +1,20 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - wallet_test.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d wnat %d", got, want)
+	}
+}
