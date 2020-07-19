31800b098316c78d75fcf09f78d21f94f8b2795b
Sungyub NA <darkerkorean@gmail.com>
2020-07-19 09:00:00 +0900

    feat(head-first-go): ch2
    shadow_ch2

---
+++ headfirst-go/ch2/shadow_ch2.go
@@ -0,0 +1,16 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - shadow_ch2.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package main
+
+func main() {
+	var int int = 12
+	var append string = "minutes of bonus footage"
+	var fmt string = "DVD"
+
+	var count int
+	var languages = append([]string{}, "Korean")
+	fmt.Println(int, append, "on", fmt, languages)
+}
