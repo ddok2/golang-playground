692c39c6dc7b5b4d76eb52900f1f2dd18c67f4ab
Sungyub NA <darkerkorean@gmail.com>
2020-07-24 09:14:20 +0900

    feat(learn-go-with-tests): maps
    write the test

--- learn-go-with-tests/maps/dictionary_test.go
+++ learn-go-with-tests/maps/dictionary_test.go
@@ -8,18 +8,39 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertString(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
-func assertString(t *testing.T, got, want string) {
+func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+/**
+ go test
+# _/Users/sung/Development/01.sung/golang-playground/learn-go-with-tests/maps [_/Users/sung/Development/01.sung/golang-playground/learn-go-with-tests/maps.test]
+./dictionary_test.go:14:10: assignment mismatch: 2 variables but dictionary.Search returns 1 values
+./dictionary_test.go:21:10: assignment mismatch: 2 variables but dictionary.Search returns 1 values
+FAIL    _/Users/sung/Development/01.sung/golang-playground/learn-go-with-tests/maps [build failed]
+*/
