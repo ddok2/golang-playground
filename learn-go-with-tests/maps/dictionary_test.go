0b0e325e74483ccb744396e1d9e6992ed53035f6
Sungyub NA <darkerkorean@gmail.com>
2020-07-30 09:09:17 +0900

    feat(learn-go-with-tests): maps
    write the test first

--- learn-go-with-tests/maps/dictionary_test.go
+++ learn-go-with-tests/maps/dictionary_test.go
@@ -27,12 +27,26 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	assertDefinition(t, dictionary, word, definition)
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
@@ -50,7 +64,13 @@ func assertDefinition(t *testing.T, dictionary Dictionary, word, definition stri
 func assertError(t *testing.T, got, want error) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got error %q want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
+	}
+	if got == nil {
+		if want == nil {
+			return
+		}
+		t.Fatal("expected to get an error.")
 	}
 }
 
@@ -61,3 +81,12 @@ func assertStrings(t *testing.T, got, want string) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+/*
+ go test
+# _/Users/sung/Development/01.sung/golang-playground/learn-go-with-tests/maps [_/Users/sung/Development/01.sung/golang-playground/learn-go-with-tests/maps.test]
+./dictionary_test.go:35:24: dictionary.Add(word, definition) used as value
+./dictionary_test.go:45:24: dictionary.Add(word, "new test") used as value
+./dictionary_test.go:47:23: undefined: ErrWordExists
+FAIL    _/Users/sung/Development/01.sung/golang-playground/learn-go-with-tests/maps [build failed]
+*/
