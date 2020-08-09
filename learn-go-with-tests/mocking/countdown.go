0197d98ec70b414c3d0b20e516c0c5d1ce31886a
Sungyub NA <darkerkorean@gmail.com>
2020-08-09 09:00:00 +0900

    feat(learn-go-with-tests): Mocking
    write enough code to make it pass

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -8,16 +8,40 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
 	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
