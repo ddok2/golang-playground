28d134676dadbfdbf5939208bf005cffc36c362f
Sungyub NA <darkerkorean@gmail.com>
2020-08-12 09:00:00 +0900

    feat(learn-go-with-tests): concurrency
    write enough code to make it pass

---
+++ learn-go-with-tests/concurrency/check_websites.go
@@ -0,0 +1,18 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - check_websites.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package concurrency
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+
+	return results
+}
