6399bd3e99636a1774573772dbfa52ac37221162
Sungyub NA <darkerkorean@gmail.com>
2020-08-21 09:00:00 +0900

    feat(learn-go-with-tests): Select
    write the test first

---
+++ learn-go-with-tests/selelct/racer_test.go
@@ -0,0 +1,20 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - racer_test.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package selelct
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	url1 := "https://www.facebook.com"
+	url2 := "https://www.sungyub.com"
+
+	want := url2
+	got := Racer(url1, url2)
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
