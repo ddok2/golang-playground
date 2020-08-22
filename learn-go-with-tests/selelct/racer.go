db1b486c80942366ea161e8c1eb6ec18be5a824a
Sungyub NA <darkerkorean@gmail.com>
2020-08-22 09:00:00 +0900

    feat(learn-go-with-tests): Select
    write the minimal amount of code for the test to run and check the failing test output

---
+++ learn-go-with-tests/selelct/racer.go
@@ -0,0 +1,10 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - racer.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package selelct
+
+func Racer(a, b string) (winner string) {
+	return
+}
