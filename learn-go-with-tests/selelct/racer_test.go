16bebf4c0aee7459e99a0686a0e60617cb465b33
Sungyub NA <darkerkorean@gmail.com>
2020-08-24 09:00:00 +0900

    feat(learn-go-with-tests): Select
    write enough code to make it pass!

--- learn-go-with-tests/selelct/racer_test.go
+++ learn-go-with-tests/selelct/racer_test.go
@@ -5,16 +5,36 @@
 
 package selelct
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	url1 := "https://www.facebook.com"
-	url2 := "https://www.google.com"
+	slowServer := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(20 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		},
+	))
+	fastServer := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		},
+	))
 
-	want := url2
-	got := Racer(url1, url2)
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
