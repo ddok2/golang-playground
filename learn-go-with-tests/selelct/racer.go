5c43242b8d729f585474472ce6b2c0ae174fc970
Sungyub NA <darkerkorean@gmail.com>
2020-08-25 09:00:00 +0900

    feat(learn-go-with-tests): Select
    refactor

--- learn-go-with-tests/selelct/racer.go
+++ learn-go-with-tests/selelct/racer.go
@@ -11,16 +11,17 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureReponseTime(a)
+	bDuration := measureReponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 	return b
+}
+
+func measureReponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
