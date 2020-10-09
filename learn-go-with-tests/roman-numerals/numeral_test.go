8aa947b02f2a0bf0549d51f3f8dfa0a7c83c5a18
Sungyub NA <darkerkorean@gmail.com>
2020-10-09 09:00:00 +0900

    feat(learn-go-with-tests): Roman Numerals
    write the test first

---
+++ learn-go-with-tests/roman-numerals/numeral_test.go
@@ -0,0 +1,17 @@
+// Copyright 2020. SUNGYUB.COM. All Rights Reserved.
+//
+// - numeral_test.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
+package roman_numerals
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	got := ConvertToRoman(1)
+	want := "I"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
