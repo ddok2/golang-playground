01861a329997c592a110b9a093b67a50c21b1ffa
Sungyub NA <darkerkorean@gmail.com>
2020-12-31 11:20:00 +0900

    feat(learn-go-with-tests): Sync
    write enough code 2make it pass

--- learn-go-with-tests/sync/sync.go
+++ learn-go-with-tests/sync/sync.go
@@ -1,1 +1,18 @@
+// Copyright 2021. SUNGYUB.COM. All Rights Reserved.
+//
+// - sync.go
+// - author: Sungyub NA <mailto: darkerkorean@gmail.com>
+
 package sync
+
+type Counter struct {
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
